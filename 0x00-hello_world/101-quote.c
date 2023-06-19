6211248af3b717fb56aa10cddc8e55f75b15ce2a
Myjimada <myjimada@92.com>
2023-06-19 16:32:40 -0700

    Hello, Word

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, strlen(message));
-    return 1;
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
